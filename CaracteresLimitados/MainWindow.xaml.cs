0e1a3ad5f3dbf3026e997f62f0bb714eb3e8ee03
Sandra Hurtado <sandra.hurtado16@iesdoctorbalmis.com>
2021-09-29 12:05:10 +0200

    Ejercicio terminado

--- CaracteresLimitados/MainWindow.xaml.cs
+++ CaracteresLimitados/MainWindow.xaml.cs
@@ -9,23 +9,25 @@ namespace CaracteresLimitados
     /// </summary>
     public partial class MainWindow : Window
     {
-        int contador = 0;
+        int contador;
+        const string CARACTERES_MAXIMOS = "/140";
         public MainWindow()
         {
             InitializeComponent();
+            
+            contadorTextBox.Text = contador.ToString() + CARACTERES_MAXIMOS;
         }
         public void ActualizarContador()
         {
             contador = textoTextBox.Text.Length;
-            contadorTextBox.Text = contador.ToString();
+            contadorTextBox.Text = contador.ToString() + CARACTERES_MAXIMOS;
+            if (contador == 140)
+                textoTextBox.IsReadOnly = true;
         }
 
         private void textoTextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
         {
             ActualizarContador();
-            if (contador >= 140)
-                textoTextBox.IsReadOnly = true;
-            
         }
     }
 }
